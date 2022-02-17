b479d04e8e002d9f29b7b7a168f93b8a60d251fc
OkGalaxy <chrislee.cl0608@gmail.com>
2022-02-17 14:47:30 -0500

    javascript

---
+++ webville/playist.js
@@ -0,0 +1,12 @@
+var button = document.getElementById("addButton");
+function handleButtonClick() {
+    alert("Button was clicked!");
+}
+window.onload = init;
+function init() {
+    var button = document.getElementById("addButton");
+    button.onclick = handleButtonClick;
+}
+function handleButtonClick() {
+    alert("Button was clicked!");
+}
